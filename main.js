d1aaeb727a874f3db8f1848e6fbe968ffe0a4b00
Semikuhl <46796866+mrvinrsk@users.noreply.github.com>
2021-10-29 11:34:12 +0200

    Eigenes Kontextmenü hinzugefügt

--- main.js
+++ main.js
@@ -111,6 +111,116 @@ document.querySelector('#navigator_bg').addEventListener('click', () => {
 /* Close Navigator on click mobile */
 document.querySelector('#navigator').querySelectorAll('li:not(#logo)').forEach(li => {
     li.addEventListener('click', () => {
-        toggleNav();
+        if (document.querySelector('#navigator').classList.contains("open") || li.id === 'hamburger') {
+            toggleNav();
+        }
     });
 });
+
+
+$(document).ready(function () {
+    $('#navigator a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        let offset = 0;
+        var target = this.hash;
+        var $target = $(target);
+
+        if (this.hash !== '') {
+            if ("scrollOffset" in document.querySelector(this.hash).dataset) {
+                offset = document.querySelector(this.hash).dataset.scrollOffset;
+            }
+        }
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - offset
+        }, 900, 'swing');
+    });
+});
+
+function hideAllContextmenus() {
+    document.querySelectorAll('.context').forEach((el) => {
+        el.classList.remove('visible');
+        el.style.top = "-300%";
+    });
+}
+
+/* Global Contextmenu */
+let global_context = document.getElementById('global_context');
+document.body.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+
+    global_context.classList.add('visible');
+
+    global_context.style.top = (e.clientY - 2) + "px";
+    global_context.style.left = (e.clientX - 2) + "px";
+});
+
+/* Link Contextmenu */
+let link_context = document.getElementById('link_context');
+document.querySelectorAll('a').forEach((a) => {
+    a.addEventListener('contextmenu', (e) => {
+        console.log(a.href);
+        e.preventDefault();
+
+        hideAllContextmenus();
+        setTimeout(() => {
+            hideAllContextmenus();
+
+            if (a.href !== '') {
+                setTimeout(() => {
+                    link_context.querySelector('#newtab').href = a.href;
+                    link_context.classList.add('visible');
+
+                    link_context.style.top = (e.clientY - 2) + "px";
+                    link_context.style.left = (e.clientX - 2) + "px";
+                }, 1);
+            }
+        }, 1);
+    });
+});
+
+
+/* Hide all contextmenus on mouseleave */
+document.querySelectorAll('.context').forEach((el) => {
+    el.addEventListener('mouseleave', (e) => {
+        hideAllContextmenus();
+    });
+});
+
+
+
+/* Copy */
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
